e0434c45846b9515db21bd1de0dd1a8c1e31c2b2
Ryely <ryely.moore@outlook.com>
2021-09-26 06:35:02 -0600

    included linkedIn in footer

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -5,6 +5,8 @@ const Footer = (props) => {
     return (
         <div className="App-footer">
             <a href="https://github.com/Rym25">GitHub</a>
+            <a href="www.linkedin.com/in/ryely-moore-452322a3">LinkedIn</a>
+            
         </div>
     );
 };
